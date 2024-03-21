746d0e91beae9a77c2969de117bdfcaf5c728eda
AndreasYager <andreas.yager@gmail.com>
2024-03-21 15:19:12 +0100

    format card

--- src/components/product/Product.js
+++ src/components/product/Product.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './Product.css'; 
-
+import { Card, CardImg, CardBody, CardTitle, CardText, Button } from 'reactstrap';
+import './Product.css';
 
 const Product = ({ product }) => {
     return (
-        <div className="product-card">
-            <img src={product.image.url} alt={product.image.alt} />
-            <h2>{product.title}</h2>
-            <p>Price: ${product.price.toFixed(2)}</p>
-                            {product.discountedPrice < product.price && (
-                                <p>Discounted Price: ${product.discountedPrice.toFixed(2)} (You save ${product.price - product.discountedPrice})</p>
-                            )}
-            <Link to={`/product/${product.id}`}>View Product</Link>
-        </div>
+        <Card className="product-card">
+            <CardImg top width="100%" src={product.image.url} alt={product.image.alt} />
+            <CardBody>
+                <CardTitle tag="h5">{product.title}</CardTitle>
+                <CardText>Price: ${product.price.toFixed(2)}</CardText>
+                {product.discountedPrice < product.price && (
+                    <CardText>Discounted Price: ${product.discountedPrice.toFixed(2)}</CardText>
+                )}
+                <Button color="primary" tag={Link} to={`/product/${product.id}`}>View Product</Button>
+            </CardBody>
+        </Card>
     );
 };
 
