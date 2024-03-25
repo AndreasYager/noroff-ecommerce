790a67a545cb8a1ef02ce6187b866591b8d86855
Andreas Yager <andreas.yager@gmail.com>
2024-03-25 13:47:20 +0100

    add carticon

--- src/components/carticon/CartIcon.js
+++ src/components/carticon/CartIcon.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import './CartIcon.css'; 
 import { Link } from 'react-router-dom';
+import cartlogo from '../../images/ecom-cart.png'
 
 
 const CartIcon = ({ itemCount }) => {
     return (
         <div className="cart-icon">
-            <Link to="/cart">Cart</Link>      
-            <span className="cart-count">{itemCount}</span>
+            <Link to="/cart">
+                <img src={cartlogo} alt="Cart" style={{ maxWidth: '70px', height: 'auto' }} />
+            </Link>      
+            {itemCount > 0 && <span className="cart-count">{itemCount}</span>}
         </div>
     );
 };
