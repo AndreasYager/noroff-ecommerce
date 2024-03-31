e44beb73f69475be39b35412e249eedeaee59645
Andreas Yager <andreas.yager@gmail.com>
2024-03-31 14:46:23 +0200

    adjust how discount is displayed

--- src/components/product/Product.js
+++ src/components/product/Product.js
@@ -1,22 +1,42 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { Card, CardImg, CardBody, CardTitle, CardText, Button } from 'reactstrap';
-import './Product.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import {
+  Card,
+  CardImg,
+  CardBody,
+  CardTitle,
+  CardText,
+  Button,
+} from "reactstrap";
+import "./Product.css";
 
 const Product = ({ product }) => {
-    return (
-        <Card className="product-card">
-            <CardImg top width="100%" src={product.image.url} alt={product.image.alt} />
-            <CardBody>
-                <CardTitle tag="h5">{product.title}</CardTitle>
-                <CardText>Price: ${product.price.toFixed(2)}</CardText>
-                {product.discountedPrice < product.price && (
-                    <CardText>Discounted Price: ${product.discountedPrice.toFixed(2)}</CardText>
-                )}
-                <Button color="primary" tag={Link} to={`/product/${product.id}`}>View Product</Button>
-            </CardBody>
-        </Card>
-    );
+  return (
+    <Card className="product-card">
+      <CardImg
+        top
+        width="100%"
+        src={product.image.url}
+        alt={product.image.alt}
+      />
+      <CardBody>
+        <CardTitle tag="h5">{product.title}</CardTitle>
+        {product.discountedPrice < product.price ? (
+          <CardText>
+            <span className="original-price">${product.price.toFixed(2)}</span>{" "}
+            <span className="discounted-price">
+              ${product.discountedPrice.toFixed(2)}
+            </span>
+          </CardText>
+        ) : (
+          <CardText>Price: ${product.price.toFixed(2)}</CardText>
+        )}
+        <Button color="primary" tag={Link} to={`/product/${product.id}`}>
+          View Product
+        </Button>
+      </CardBody>
+    </Card>
+  );
 };
 
 export default Product;
