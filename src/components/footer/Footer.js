7ec4a0baf1efcb5831283e5433b1c098a941adab
Andreas Yager <andreas.yager@gmail.com>
2024-03-31 14:45:44 +0200

    format and style footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,21 +1,40 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './Footer.css'; 
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Footer.css";
+import { Container, Row, Col } from "reactstrap";
 
 const Footer = () => {
-    return (
-        <footer className="footer">
+  return (
+    <footer className="footer mt-4">
+      <Container>
+        <Row>
+          <Col className="text-center">
             <nav className="footer-nav">
-                <Link to="/">Home</Link>
-                <Link to="/products">Products</Link>
-                <Link to="/contact">Contact</Link>
+              <Link to="/" className="nav-link">
+                Home
+              </Link>
+              <Link to="/#search" className="nav-link">
+                Search
+              </Link>
+              <Link to="/contact" className="nav-link">
+                Contact
+              </Link>
             </nav>
+          </Col>
+        </Row>
+        <Row>
+          <Col className="text-center">
             <div className="footer-info">
-                <p>&copy; {new Date().getFullYear()} Noroff E-Commerce. All rights reserved.</p>
-                {/* Add any additional info you want here */}
+              <p>
+                &copy; {new Date().getFullYear()} Noroff E-Commerce. All rights
+                reserved.
+              </p>
             </div>
-        </footer>
-    );
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  );
 };
 
 export default Footer;
