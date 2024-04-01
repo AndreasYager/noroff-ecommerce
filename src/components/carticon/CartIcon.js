3c5d16f42d2b0c6be13161cafc7d66eb6c85f00a
Andreas Yager <andreas.yager@gmail.com>
2024-04-01 17:01:53 +0200

    display cart total in header

--- src/components/carticon/CartIcon.js
+++ src/components/carticon/CartIcon.js
@@ -1,18 +1,25 @@
-import React from 'react';
-import './CartIcon.css'; 
-import { Link } from 'react-router-dom';
-import cartlogo from '../../images/ecom-cart.png'
+import React from "react";
+import { useCart } from "../../CartContext";
+import "./CartIcon.css";
+import { Link } from "react-router-dom";
+import cartlogo from "../../images/ecom-cart.png";
 
+const CartIcon = () => {
+  const { cart } = useCart();
+  const itemCount = cart.reduce((total, item) => total + item.quantity, 0);
 
-const CartIcon = ({ itemCount }) => {
-    return (
-        <div className="cart-icon">
-            <Link to="/cart">
-                <img src={cartlogo} alt="Cart" style={{ maxWidth: '70px', height: 'auto' }} />
-            </Link>      
-            {itemCount > 0 && <span className="cart-count">{itemCount}</span>}
-        </div>
-    );
+  return (
+    <div className="cart-icon">
+      <Link to="/cart">
+        <img
+          src={cartlogo}
+          alt="Cart"
+          style={{ maxWidth: "70px", height: "auto" }}
+        />
+      </Link>
+      {itemCount > 0 && <span className="cart-count">{itemCount}</span>}
+    </div>
+  );
 };
 
 export default CartIcon;
