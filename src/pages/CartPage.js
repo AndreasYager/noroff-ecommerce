fa80d60eeeddb0c1a16fec81ef1b47d3d108f36e
Andreas Yager <andreas.yager@gmail.com>
2024-04-01 17:04:33 +0200

    create cart

--- src/pages/CartPage.js
+++ src/pages/CartPage.js
@@ -1,7 +1,46 @@
-import React from 'react';
+import React from "react";
+import { useCart } from "../CartContext";
+import CheckoutForm from "../components/CheckoutForm";
 
 const CartPage = () => {
-  return <div>Home Page Content</div>;
+  const { cart, removeFromCart } = useCart();
+
+  const calculateTotal = (cartItems) => {
+    return cartItems.reduce(
+      (total, item) => total + item.price * item.quantity,
+      0
+    );
+  };
+
+  const handleRemoveFromCart = (productId) => {
+    removeFromCart(productId);
+  };
+
+  if (cart.length === 0) {
+    return <p>Your cart is empty</p>;
+  }
+
+  return (
+    <div>
+      <h2>Your Cart</h2>
+      {cart.map((product) => (
+        <div key={product.id}>
+          <h3>{product.title}</h3>
+          <p>Price: ${product.price.toFixed(2)}</p>
+          <p>Quantity: {product.quantity}</p>
+          <button onClick={() => handleRemoveFromCart(product.id)}>
+            Remove
+          </button>
+        </div>
+      ))}
+      <div>
+        <div>
+          <h3>Total: ${calculateTotal(cart).toFixed(2)}</h3>
+          <CheckoutForm />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CartPage;
